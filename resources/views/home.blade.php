6857b7eaab8b14dfd846d720a98d79ffc69b8f5a
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-02-02 22:39:07 +0100

    header + jumbo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+
+@section('pageTitle')
+  Home
+@endsection
+
+@section('pageContent')
+  @include('partials.jumbo')
+  <div class="main-series">
+    
+  </div>
+@endsection
