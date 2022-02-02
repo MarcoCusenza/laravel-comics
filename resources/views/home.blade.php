c166cf7d9289fd5c3d5721439269bbea0b94011b
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-02-02 23:15:01 +0100

    main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.base')
 
 @section('pageTitle')
-  Home
+    Home
 @endsection
 
 @section('pageContent')
-  @include('partials.jumbo')
-  <div class="main-series">
-    
-  </div>
+    @include('partials.jumbo')
+    <div class="main-series">
+        <div class="container">
+            <div class="cur-ser blue-box">current series</div>
+            <div class="container-card">
+                @foreach ($comics as $comic)
+                    <div class="card">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        <div class="title">{{ $comic['title'] }}</div>
+                    </div>
+                @endforeach
+            </div>
+            <a href="#" class="blue-box">load more</a>
+        </div>
+    </div>
 @endsection
