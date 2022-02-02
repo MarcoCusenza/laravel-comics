df1a678304acfca19418498f4e7240a09759b9e7
Marco Cusenza <marco.cusenza.me@gmail.com>
2022-02-02 23:27:15 +0100

    footer top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,36 @@
 <footer>
-  
+    <div class="footer-top">
+        <div class="container">
+            <a class="card" href="#">
+                <div class="icon">
+                    <img src="/img/buy-comics-digital-comics.png" alt="digital comics" />
+                </div>
+                <div class="section-name">digital comics</div>
+            </a>
+            <a class="card" href="#">
+                <div class="icon">
+                    <img src="/img/buy-comics-merchandise.png" alt="merchandise" />
+                </div>
+                <div class="section-name">merchandise</div>
+            </a>
+            <a class="card" href="#">
+                <div class="icon">
+                    <img src="/img/buy-comics-subscriptions.png" alt="subscriptions" />
+                </div>
+                <div class="section-name">subscriptions</div>
+            </a>
+            <a class="card" href="#">
+                <div class="icon">
+                    <img src="/img/buy-comics-shop-locator.png" alt="shop locator" />
+                </div>
+                <div class="section-name">shop locator</div>
+            </a>
+            <a class="card" href="#">
+                <div class="icon">
+                    <img src="/img/buy-dc-power-visa.svg" alt="digital comics" />
+                </div>
+                <div class="section-name">DC power visa</div>
+            </a>
+        </div>
+    </div>
 </footer>
